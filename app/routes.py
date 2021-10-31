ef95e154195ec72c5e219ef8d8c6d5dae05bf7ee
louysotopo <louysotopo123@gmail.com>
2021-10-31 13:58:07 -0500

    certificados

--- app/routes.py
+++ app/routes.py
@@ -75,6 +75,7 @@ from app.resources.curso_seguimiento.curso_seguimiento_api import Curso_seguimie
 from app.resources.curso_seguimiento.curso_seguimiento_add_api import Curso_seguimiento_add_api
 from app.resources.curso_seguimiento.curso_seguimiento_docentes_api import Curso_seguimiento_docentes_api
 
+from app.resources.certificado.certificado_send_api import Certificado_export
 
 from app.resources.login.login import Login_api
 
@@ -155,6 +156,7 @@ api.add_resource(Curso_seguimiento_api,'/curso_seguimiento/','/curso_seguimiento
 api.add_resource(Curso_seguimiento_add_api, '/curso_seguimiento/create/', '/curso_seguimiento/create/<string:id>')
 api.add_resource(Curso_seguimiento_docentes_api,'/curso_seguimiento/docentes/','/curso_seguimiento/docentes/<string:id>')
 
+api.add_resource(Certificado_export,'/certificado_export/','/certificado_export/<string:id>')
 
 api.add_resource(Login_api,'/login/','/login/<string:id>')
 
