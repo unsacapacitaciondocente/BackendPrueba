6949d73475149bbbbda9a9b2890de8c475b8b675
louysotopo <louysotopo123@gmail.com>
2021-10-31 14:48:01 -0500

    Update certificado_send_api.py

--- app/resources/certificado/certificado_send_api.py
+++ app/resources/certificado/certificado_send_api.py
@@ -8,49 +8,11 @@ import numpy as np
 class Certificado_export(Resource):
     
     def get(self):
-
-        try:
-            with NamedTemporaryFile() as temp:
-                image_procesed = 255*np.ones((600,1000,3),dtype=np.uint8)
-
-                texto_ = "nombre"
-                ubicacion_ = (5,50)
-                font_ = cv.FONT_HERSHEY_PLAIN
-                tamañoLetra_ = 1
-                colorLetra_ = (0,0,0)
-                grosorLetra_ = 1
-
-                cv.putText(image_procesed, texto_, ubicacion_, font_, tamañoLetra_, colorLetra_, grosorLetra_)
-                texto_ = "something else"
-                ubicacion_ = (5,70)
-                cv.putText(image_procesed, texto_, ubicacion_, font_, tamañoLetra_, colorLetra_, grosorLetra_)
-                iName = "".join([str(temp.name),".PNG"])
-                cv.imwrite(iName,image_procesed)
-                return send_file(iName,mimetype='image/png') 
-        except Exception as e:
-            return jsonify({"message":str(e)})
-        #return jsonify({"message":"Solo POST"})  
+        return jsonify({"message":"Solo POST"})  
         
     def post(self):
-        import cv2 as cv
-        import numpy as np
         try:
             with NamedTemporaryFile() as temp:
-                image_procesed = 255*np.ones((600,1000,3),dtype=np.uint8)
-
-                texto_ = "nombre"
-                ubicacion_ = (5,50)
-                font_ = cv.FONT_HERSHEY_PLAIN
-                tamañoLetra_ = 1
-                colorLetra_ = (0,0,0)
-                grosorLetra_ = 1
-
-                cv.putText(image_procesed, texto_, ubicacion_, font_, tamañoLetra_, colorLetra_, grosorLetra_)
-                texto_ = "something else"
-                ubicacion_ = (5,70)
-                cv.putText(image_procesed, texto_, ubicacion_, font_, tamañoLetra_, colorLetra_, grosorLetra_)
-                iName = "".join([str(temp.name),".PNG"])
-                cv.imwrite(iName,image_procesed)
-                return send_file(iName,mimetype='image/png') 
+                return jsonify({"message":"holi"}) 
         except Exception as e:
             return jsonify({"message":str(e)})
