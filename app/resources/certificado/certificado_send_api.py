8b81a1c364582b223e9af1ff9e0b0a2d36ba3cf0
louysotopo <louysotopo123@gmail.com>
2021-10-31 15:15:37 -0500

    Update certificado_send_api.py

--- app/resources/certificado/certificado_send_api.py
+++ app/resources/certificado/certificado_send_api.py
@@ -13,6 +13,20 @@ class Certificado_export(Resource):
     def post(self):
         try:
             with NamedTemporaryFile() as temp:
-                return jsonify({"message":"holi"}) 
+                image_procesed = 255*np.ones((600,1000,3),dtype=np.uint8)
+                texto_ = "nombre"
+                ubicacion_ = (5,50)
+                font_ = cv.FONT_HERSHEY_PLAIN
+                tamañoLetra_ = 1
+                colorLetra_ = (0,0,0)
+                grosorLetra_ = 1
+
+                cv.putText(image_procesed, texto_, ubicacion_, font_, tamañoLetra_, colorLetra_, grosorLetra_)
+                texto_ = "something else"
+                ubicacion_ = (5,70)
+                cv.putText(image_procesed, texto_, ubicacion_, font_, tamañoLetra_, colorLetra_, grosorLetra_)
+                iName = "".join([str(temp.name),".PNG"])
+                cv.imwrite(iName,image_procesed)
+                return send_file(iName,mimetype='image/png') 
         except Exception as e:
             return jsonify({"message":str(e)})
